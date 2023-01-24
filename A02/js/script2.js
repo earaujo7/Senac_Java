86843588cd0ab3a128fbca73bf001783f48a60dd
EA07 <er@gmail.com>
2023-01-24 17:30:05 -0300

    Final Aula 02

---
+++ A02/js/script2.js
@@ -0,0 +1,11 @@
+console.log("Hello World!");
+
+var foo = "bar";
+console.log(foo);
+
+var thisVar = 'First value';
+var thatVar = 'Second value';
+console.log("thisVar:", thisVar, "and thatVar:", thatVar);
+
+var greet = "Hello!", who = "World";
+console.log("%S, %S!", greet, who);
